f8bce376897e8fbcafdab4c26cb9e41ab08a2665
Donatello <donatelo120193@gmail.com>
2017-09-04 01:08:04 -0400

    little change on home message

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -15,12 +15,12 @@ export class HomePage {
   	this.afAuth.authState.subscribe(data => {
   	  if(data && data.email && data.uid){
 	  	  this.toast.create({
-	  	  	message:'Welcome to APP_NAME, ${data.email}',
+	  	  	message:`Welcome to APP_NAME, ${data.email}`,
 	  	  	duration: 3000
 	  	  }).present();
 	  }else{
 	  	  this.toast.create({
-	  	  	message:'Could nod find authentication details',
+	  	  	message:`Could nod find authentication details`,
 	  	  	duration: 3000
 	  	  }).present();
 	  	
