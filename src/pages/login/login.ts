3e859072d536b62365504c427dcc5bbf7fc1c88c
Donatello <donatelo120193@gmail.com>
2017-09-04 14:18:34 -0400

    simple exception when login fail

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams , ToastController} from 'ionic-angular';
 import { User } from "../../models/user";
 import { AngularFireAuth } from 'angularfire2/auth';
 
@@ -10,17 +10,26 @@ import { AngularFireAuth } from 'angularfire2/auth';
 })
 export class LoginPage {
   user = {} as User; 
-  constructor(private afAuth: AngularFireAuth, public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private afAuth: AngularFireAuth,private toast: ToastController, public navCtrl: NavController, public navParams: NavParams) {
   }
   async login(user: User ){
     try { 
       const result = await this.afAuth.auth.signInWithEmailAndPassword(user.email,user.password);  
       if (result) {
         this.navCtrl.setRoot('HomePage');
+      }else{
+        this.toast.create({
+          message:`Fail Login`,
+          duration: 3000
+        }).present();
       }
     }
     catch(e){
       console.error(e);
+      this.toast.create({
+          message:`Fail Login`,
+          duration: 3000
+        }).present();
     }
   }
   register(){
