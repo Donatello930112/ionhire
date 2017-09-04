af3d76d16b4ea68d97470108d52ae2af8157665a
Donatello <donatelo120193@gmail.com>
2017-09-04 01:05:41 -0400

    simple login implementation

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG  = {
+	apiKey: "AIzaSyAbtXGHZLATXP2swVDQ0Uy7vnfjq4B3grQ",
+    authDomain: "ionhire.firebaseapp.com",
+    databaseURL: "https://ionhire.firebaseio.com",
+    projectId: "ionhire",
+    storageBucket: "ionhire.appspot.com",
+    messagingSenderId: "218732386361"
+};
